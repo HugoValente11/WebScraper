1d6992d7bcf5a4b97f184c0f07da6e17ece159f1
Hugo Valente <hugomika68@gmail.com>
2020-11-02 11:37:32 +0100

    Add images scraping for Google

---
+++ images.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 10:33:50 2020
+
+@author: Hugo
+"""
+from bs4 import BeautifulSoup
+import requests
+from PIL import Image
+from io import BytesIO
+
+# Making search and downloading HTTML
+query = input("What do you want to search?\n")
+
+
+# build the google query
+search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
+
+r = requests.get(search_url.format(q=query))
+
+# Parsing soup
+soup = BeautifulSoup(r.text)
+# links = soup.find_all("a", {"class": "mimg"})
+links = soup.find_all("img")
+
+i = 1
+for item in links:
+    if item.attrs["src"] and "http" in item.attrs["src"]:
+        img_obj = requests.get(item.attrs["src"])
+        print("Status_code:", img_obj.status_code)
+    
+        if img_obj.status_code == 200:     
+            print("Getting:", item.attrs["src"])
+            path = f"./{query}{i}." + img.format.lower()
+
+            i += 1
+            img = Image.open(BytesIO(img_obj.content))
+            img.save(f"./scraped_images/{path}", img.format)
+        else:
+            print("An error occured")
+        
