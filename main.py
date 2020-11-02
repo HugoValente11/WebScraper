a350b757184711f34aa201eeede7371b7fa71fc5
Hugo Valente <hugomika68@gmail.com>
2020-11-02 10:06:15 +0100

    Add find parent and siblings

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Making search and downloading HTTML
 search = input("What do you want to search?\n")
 params = {"q" : search}
 r = requests.get("https://www.bing.com/search", params = params)
+
+# Parsing soup
 soup = BeautifulSoup(r.text, "html.parser")
-print(soup.prettify())
+# print(soup.prettify())
 
 results = soup.find("ol", {"id": "b_results"})
 links = results.find_all("li", {"class": "b_algo"})
@@ -17,5 +20,16 @@ for item in links:
     if item_text and item_link:
         print(item_text)
         print(item_link)
+        
+        # Find greatparent's description
+        print("Parent description:", item.find("a").parent.parent.find("p").text)
+        
+        children = item.children
+        for child in children:
+            print("Child:", child)
+            
+        children = item.find("h2")
+        print(f"\n\nChild and sibling: \nH2: {children}\n\nDiv: {children.next_sibling}")
+        
 
     
